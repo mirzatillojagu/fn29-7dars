d04960d23be9e6599f02af1fc0dbefd78acc936e
mirzatillojagu <imirzatillo0@gmail.com>
2024-10-04 09:17:52 +0500

    vazifa

---
+++ js/index,.js
@@ -0,0 +1,162 @@
+// 1
+// console.log(Math.floor(Math.random() * 100));
+
+// 4
+// let num = +prompt("son")
+// console.log(Math.floor(num));
+// console.log(Math.round(num));
+
+//////////////////////////////////Ternary operator//////////////////////////
+
+// 1
+// let num = +prompt("son")
+
+// num > 0 ? console.log("musbat son"): console.log("musbat son emas");
+
+// 2
+// let num = +prompt("yoshingizni kriting")
+
+// num >= 18 ? console.log("ruxsat etilgan"): console.log("ruxsat etilmagan");
+
+// 3
+// let num = +prompt("sonni kriting")
+
+// num % 2 == 0 ? console.log("juft son") : console.log("toq son");
+
+///////////////////////////for sikl operator/////////////////////////////////
+
+// 1
+//  for(let i = 0; i <= 20; i+=2){
+//      console.log(i);
+//  }
+
+// 2
+
+// let sum = 0;
+// for(let i=0; i <= 10; i++){
+//     let num = +prompt(`${i}sonni kriting`);
+//     if (num % 2 == 0) {
+//         sum += num
+//     }
+// }
+
+// console.log(sum);
+
+// 3
+// for(let i=1; i <= 100; i++){
+//     if (i > 30 && i < 50) {
+//         console.log(i);
+
+//     }
+// }
+
+
+
+// 4
+
+// let musbat = 0;
+// let manfiy = 0;
+// for (let i = 1; i <= 4; i++) {
+//   let num = +prompt(`${i}sonni kriting`);
+//   if (i > 0) {
+//     musbat ++;
+//   } else {
+//     manfiy ++;
+//   }
+// }
+// console.log(musbat);
+// console.log(manfiy);
+
+
+// 6
+// let sum = 0;
+// for(let i=1; i <= 50; i++){
+//     if (i % 2 == 1) {
+//         sum++
+//     }
+// }
+
+// console.log(sum);
+
+
+
+// 7
+// let res = 0;
+// for(let i=1; i <= 5; i++){
+//     let num = +prompt(`${i} odamning yoshni kriting`)
+//     if (i >= 18) {
+//        res = "voyaga yetgan"
+//     }else{
+//         res = "voyaga yetmagan"
+//     }
+// }
+// console.log(res);
+
+
+// 8
+// let count = 0;
+// for(let i=1; i <= 100; i++){
+//     if (i % 2 == 0) {
+//         count++
+//     }
+// }
+
+// console.log(count);
+
+// 9
+// let count = 0;
+// for(let i=1; i <= 10; i++){
+//     let num = +prompt(`${i} sonni kriting`)
+//     if (i > 5 && i < 10) {
+//         count++;
+//     }
+// }
+// console.log(count);
+
+
+// 10
+// for(let i=1; i <= 10; i++){
+//     let num = +prompt(`${i} sonni kriting`)
+//     if (i > 0) {
+//         console.log(num);
+        
+//     }
+// }
+
+
+// 11
+// for(let i=1; i <= 15; i++){
+//     let num = +prompt(`${i} sonni kriting`)
+//     if (i > 0 || i < 0) {
+//         console.log(num);
+        
+//     }
+// }
+
+
+// 12
+// for(let i=1; i <= 50; i++){
+//     if (i % 3 == 0) {
+//         console.log(i);
+        
+//     }
+// }
+
+// 14
+// for(let i=1; i <= 10; i++){
+//     let num = +prompt(`${i} sonni kriting`)
+//     if (num >= 50) {
+//         console.log(num);
+        
+//     }
+// }
+
+
+// 15
+// for(let i=1; i <= 10; i++){
+//     let num = +prompt(`${i} sonni kriting`)
+//     if (num === 0) {
+//         console.log(num);
+        
+//     }
+// }
